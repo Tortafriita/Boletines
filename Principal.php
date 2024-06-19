1ada6e343081b229a089ecdf798e8a1d89ac973a
Tortafriita <154385495+Tortafriita@users.noreply.github.com>
2024-06-19 16:28:09 -0300

    Add files via upload

---
+++ Principal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylee.css">
+</head>
+<body>
+    <header class="header">
+       <div class="logo">
+        <img src="../boletines/img/logoc.jpg" width="80px" height="50px" >
+       </div>
+       <button class="Aabrir-menu">abrir</button>
+       <nav>
+       <ul class="nav-links">
+            <li><a href=""></a>Inicio</li>
+            <li><a href=""></a>Panchitolex</li>
+            <li><a href=""></a>negrajuana</li>
+        </ul>
+       </nav>
+       <a href="" class="btn"><button>Contacto</button></a>
+    </header>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,21 @@
+<html lang="es">
+<head>
+<meta http-equiv="content-Type" content="text/html; charset=utf-8">
+<title>Boletines</title>
+<link rel="stylesheet" type="text/css" href= "..\boletines\css\Login.css">
+<link rel="icon" href="img\logoc.jpg" type="image/png" size="16x16">
+</head>
+<body>
+    <div class="loginContainer">
+        <div class="loginInfoContainer">
+            <h1 class="title">INICIAR SESIÓN:</h1>
+        <img src="img\logoc.jpg">
+    <form action= 'metadatos.php' method = 'post' class="inputContainer">
+        <input class="input" type="text" name= 'user' placeholder="Nombre de usuario">
+        <input class="input" type="password" name= 'pass' placeholder="Contraseña">
+        <button class="btn">Ingrese</button>
+    </form>
+    </div>
+    </div>
+</body>
+</html>
