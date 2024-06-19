37437a35fb4e5c722f84da28030113864dad5415
Brii222 <bbrisa115@gmail.com>
2024-06-19 18:52:29 -0300

    Add files via upload

---
+++ cicloBasico.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ciclo Básico</title>
+    <link rel="stylesheet" href="..\boletines\css\cicloBasico.css">
+</head>
+<body>
+    <aside>
+        <input type="radio" name="content" id="content1" checked>
+        <label for="content1">Contenido 1</label>
+        <input type="radio" name="content" id="content2">
+        <label for="content2">Contenido 2</label>
+        <input type="radio" name="content" id="content3">
+        <label for="content3">Contenido 3</label>
+    </aside>
+
+    <main>
+        <div class="content" id="content-1">
+            <p>Este es el contenido 1.</p>
+        </div>
+        <div class="content" id="content-2">
+            <p>Este es el contenido 2.</p>
+        </div>
+        <div class="content" id="content-3">
+            <p>Este es el contenido 3.</p>
+        </div>
+    </main>
+
+    <div class="color">hola</div>
+</body>
+</html>
