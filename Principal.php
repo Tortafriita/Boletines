c1afa515a17b115edab4b218adf405e3d2543a30
vaaAlenrrrr <reichartvalen@gmail.com>
2024-06-21 18:40:45 -0300

    Add files via upload

--- Principal.php
+++ Principal.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="stylee.css">
+    <link rel="stylesheet" href="sStyle.css">
 </head>
 <body>
     <header class="header">
@@ -21,5 +21,12 @@
        </nav>
        <a href="" class="btn"><button>Contacto</button></a>
     </header>
+    <div class="center">
+    
+    <a href="info.php"><img src="img/info.jpeg" height="200px" width="200px" class="botones"></a>
+    <a href="cicloBasico.php"><img src="img/ciclo.jpeg" height="200px" width="200px" class="botones"></a>
+    <a href="elec.php"><img src="img/elec.jpeg" height="200px" width="200px" class="botones"></a>
+</div>
+
 </body>
 </html>
